802e1d5ac4022232429cdbfedb0a3bbbda443ad3
nagerenxiong <527935649@qq.com>
2016-08-22 18:03:14 +0800

    0822

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,10 @@ router.get('/', function(req, res, next) {
 router.get('/addAd', function(req, res, next) {
   res.render('addAd', { title: '添加广告' });
 });
+router.get('/addCategory', function(req, res, next) {
+  res.render('addCategory', { title: '添加分类' });
+});
+router.get('/addType', function(req, res, next) {
+  res.render('addType', { title: '添加类型' });
+});
 module.exports = router;
