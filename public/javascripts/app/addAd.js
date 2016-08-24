1f78d2cf38d04ef88359781044d7a2cc9159b13a
nagerenxiong <527935649@qq.com>
2016-08-24 18:14:10 +0800

    0824

--- public/javascripts/app/addAd.js
+++ public/javascripts/app/addAd.js
@@ -1,19 +1,23 @@
 $("#submit").click(function() {
+	var catId=$("#ggLei").val();
 	var name = $.trim($("#name").val());
 	if (name == "") {
-		layer.tips('请输入类型名称', '#submit', {
+		layer.tips('请输入广告名称', '#submit', {
 					tips: [1, 'red'],
 					time: 3000
 				});
 		return;
 	}
 	$.ajax({
-		url: '/api/addType',
+		url: '/api/addAd',
 		type: 'post',
 		data: {
 			name: name,
 			html:$("#html").val(),
-			js:$("#js").val()
+			js:$("#js").val(),
+			css:$("#css").val(),
+			boxId:$("#boxId").attr('data-boxId'),
+			catId:catId
 		},
 		success: function(result) {
 			if (result.affectedRows== 1) {
@@ -21,7 +25,9 @@ $("#submit").click(function() {
 					tips: [1, '#3595CC'],
 					time: 3000
 				});
-				$("#name").val('').focus();
+				setTimeout(function(){
+				window.location.reload();
+				},3500)
 			}else if(result=="2")
 			{
 				layer.tips('已经存在的类型', '#submit', {
@@ -37,4 +43,14 @@ $("#submit").click(function() {
 			}
 		}
 	})
+})
+$("#imgGG").click(function(){
+	layer.open({
+	  type: 1,
+	  skin: 'layui-layer-demo', //样式类名
+	  shift: 2,
+	  area: ['600px', '600px'],
+	  shadeClose: true, //开启遮罩关闭
+	  content: $('#photoDialog')
+	});
 })
--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,73 @@ var express = require('express');
 var router = express.Router();
 var fun = require('../model/mysqlBaseFun');
 var moment = require("moment");
+var multiparty = require('multiparty');
+var fs = require('fs');
+var util = require('util');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	res.render('index', {
 		title: '首页'
 	});
 });
-router.get('/addAd', function(req, res, next) {
-	res.render('addAd', {
-		title: '添加广告'
+
+router.get('/upload', function(req, res, next) {
+	res.render('upload',{files:false});
+});
+router.post('/upload', function(req, res, next) {
+	//生成multiparty对象，并配置上传目标路径
+	var form = new multiparty.Form({
+		uploadDir: './public/images/upload/'
 	});
+	//上传完成后处理
+	form.parse(req, function(err, fields, files) {
+		var filesTmp = JSON.stringify(files, null, 2);
+
+		if (err) {
+			console.log('parse error: ' + err);
+		} else {
+			// console.log('parse files: ' + filesTmp);
+			// console.log(111111111111111111111111111)
+			// console.log(files.inputFile)
+			// var inputFile = files.inputFile[0];
+			// var uploadedPath = inputFile.path;
+			// var dstPath = './file' + inputFile.originalFilename;
+			// //重命名为真实文件名
+			// fs.rename(uploadedPath, dstPath, function(err) {
+			// 	if (err) {
+			// 		console.log('rename error: ' + err);
+			// 	} else {
+			// 		console.log('rename ok');
+			// 	}
+			// });
+		}
+		// res.writeHead(200, {
+		// 	'content-type': 'text/plain;charset=utf-8'
+		// });
+		// res.write('received upload:\n\n');
+		// res.end(util.inspect({
+		// 	fields: fields,
+		// 	files: filesTmp
+		// }));
+		console.log(fields);
+		console.log(files);
+		res.render('upload',{files:files.userfile[0]["path"]});
+	});
+});
+router.get('/addAd', function(req, res, next) {
+	fun.query("select * from  category", function(rows) {
+		fun.query("SELECT boxId FROM  adv  ORDER BY id DESC LIMIT 1 ", function(rows1) {
+			console.log(rows1)
+			var boxId = 1;
+			if (rows1.length != 0)
+				boxId = rows1[0].boxId * 1 + 1
+			res.render('addAd', {
+				title: '添加广告',
+				categoryList: rows,
+				boxId: boxId
+			});
+		})
+	})
 });
 router.get('/addCategory', function(req, res, next) {
 	res.render('addCategory', {
@@ -35,22 +92,25 @@ router.get('/addType', function(req, res, next) {
 		title: '添加类型'
 	});
 });
-router.post('/api/addType', function(req, res, next) {
+router.post('/api/addAd', function(req, res, next) {
 	var name = req.body.name;
-	var html=req.body.html;
-	var js=req.body.js;
-	fun.query("select * from  type where name=('" + name + "')", function(rows) {
+	var html = req.body.html;
+	var js = req.body.js;
+	var css = req.body.css;
+	var boxId = req.body.boxId;
+	var catId = req.body.catId;
+	console.log(req.body)
+	fun.query("select * from  adv where name=('" + name + "')", function(rows) {
 		if (rows.length > 0) {
 			res.send("2");
 		} else {
 			var curTime = moment().format('YYYY-MM-DD HH:mm:ss');
-			fun.query("insert into type(name,time,html,js) values ('" + name + "','" + curTime + "','"+html+"','"+js+"')", function(rows1) {
+			fun.query("insert into adv(name,time,html,js,css,boxId,catId) values ('" + name + "','" + curTime + "','" + html + "','" + js + "','" + css + "','" + boxId + "','" + catId + "')", function(rows1) {
 				res.send(rows1);
 			});
 		}
 	})
 });
-
 
 
 
@@ -79,7 +139,7 @@ router.post('/api/updateCategory', function(req, res, next) {
 router.post('/api/deleteCategory', function(req, res, next) {
 	var idList = req.body['idList[]'];
 	console.log(typeof(idList));
-	if (typeof(idList)=="string")
+	if (typeof(idList) == "string")
 		idList = idList[0];
 	else
 		idList = idList.join(',');
